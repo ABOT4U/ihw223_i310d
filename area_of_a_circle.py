bd18a204dc7b196c4ffdf5ec8ac71ccbec6f2c41
Ivan Wu <ivanwu@wireless-10-155-241-232.public.utexas.edu>
2026-04-08 16:22:52 -0500

    a

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -1,12 +1,12 @@
 def compute_area_of_circle(radius):
-	pi = 3.14
+	pi = 3.1415926
 	area = pi * radius * radius
 	return area
 
-radius1 = 30
+radius1 = 2
 area1 = compute_area_of_circle(radius1)
 print(f"The area of circle with radius {radius1} is: {area1}")
 
-radius2 = 40
+radius2 = 1
 area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
